649abec147c953e64a98934b1c48f2a7b49c9135
Elmira <elmira.sarrafi@gmail.com>
2024-08-27 18:54:48 -0400

    Revert "added more pages-empty"
    This reverts commit 376798c3601351a17e0a9204844f20969954ab2c.

--- web.py
+++ web.py
@@ -10,8 +10,7 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to <b>increase</b> your productivity.",
-         unsafe_allow_html=True)
+st.write("This app is to increase your productivity.")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
